965e38591b90dd21bafa8e4509fb35884ab1481f
pbarbero <pbarberoiriarte@gmail.com>
2017-11-04 16:04:56 +0100

    Create solution sln

---
+++ tennis.tests/TennisUnitTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using tennis;
+
+namespace tennis.tests
+{
+    [TestClass]
+    public class TennisTestsClass
+    {
+        // private readonly PrimeService _primeService;
+
+        // public PrimeService_IsPrimeShould()
+        // {
+        //     _primeService = new PrimeService();
+        // }
+
+        // [TestMethod]
+        // public void ReturnFalseGivenValueOf1()
+        // {
+        //     var result = _primeService.IsPrime(1);
+
+        //     Assert.IsFalse(result, "1 should not be prime");
+        // }
+    }
+}
